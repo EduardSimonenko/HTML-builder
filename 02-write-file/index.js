988bdbd00f7999c6241df55b0bcd63dda17dac44
EduardSimonenko <90136070+EduardSimonenko@users.noreply.github.com>
2022-05-23 16:38:17 +0300

    feat: add second task solution

---
+++ 02-write-file/index.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+
+
+fs.createWriteStream(path.join(__dirname, 'result.txt'));
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+console.log('Write the text you want to add');
+
+rl.on('line', (input) => {
+  fs.appendFile(
+    path.join(__dirname, 'result.txt'),
+    input + '\n',
+    err => {
+      if (err) throw err;
+    }
+  );
+  if(input == 'exit'){
+    console.log('Good luck, my friend!');
+    rl.close();
+  }
+});
+
+rl.on('SIGINT', () => {
+  console.log('Good luck, my friend!');
+  rl.close();
+});
+
