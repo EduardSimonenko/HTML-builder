bfe8660058ee9f5d5add72d6b508436089295859
EduardSimonenko <90136070+EduardSimonenko@users.noreply.github.com>
2022-05-23 15:02:54 +0300

    feat: add first task solition

---
+++ 01-read-file/index.js
@@ -0,0 +1,9 @@
+const path = require('path');
+const fs = require('fs');
+
+const stream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
+
+let result = '';
+
+stream.on('data', chunk => result += chunk);
+stream.on('end', () => console.log(result));
