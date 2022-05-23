b196697180fec34075cb339c3392d94c0ce0486d
EduardSimonenko <90136070+EduardSimonenko@users.noreply.github.com>
2022-05-23 20:38:35 +0300

    feat: add fifth task solution

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const path = require('path');
+
+
+fs.writeFile(
+  path.join(__dirname, 'project-dist', 'bundle.css'),
+  '',
+  (err) => {
+    if (err) throw err;
+  }
+);
+
+fs.readdir(path.join(__dirname, 'styles'), (err, files) => {
+  if (err) {
+    console.log(err);
+  } else {
+    files.forEach(file => {
+      if (path.extname(file) == '.css') {
+
+        const stream = fs.createReadStream(path.join(__dirname, 'styles', file), 'utf-8');
+
+        let result = '';
+
+        stream.on('data', chunk => result += chunk);
+        stream.on('end', () => fs.appendFile(
+          path.join(__dirname, 'project-dist', 'bundle.css'),
+          result,
+          err => {
+            if (err) throw err;
+          })
+        );
+
+      }
+      // fs.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file), fs.constants.COPYFILE_EXCL, (err) => {
+      //   if (err) throw err;
+      // });
+    });
+  }
+});
+
+
